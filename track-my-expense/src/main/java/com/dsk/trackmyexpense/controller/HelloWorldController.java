1ae28da84ca6d9c449fe380c797cb24a640d76da
Karthik <krishnakarthik.adk@gmail.com>
2016-07-06 23:30:39 +0530

    Unnecessary code removed

--- track-my-expense/src/main/java/com/dsk/trackmyexpense/controller/HelloWorldController.java
+++ track-my-expense/src/main/java/com/dsk/trackmyexpense/controller/HelloWorldController.java
@@ -2,7 +2,6 @@ package com.dsk.trackmyexpense.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,20 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HelloWorldController {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String sayHello(ModelMap modelMap)
-	{
-		modelMap.addAttribute("greeting", "Spring 4.3 MVC");
-		return "welcome";
-	}
-	
-	@RequestMapping(value="/helloagain", method = RequestMethod.GET)
-	public String sayHelloAgain(ModelMap modelMap)
-	{
-		modelMap.addAttribute("greeting", "Hello from Spring 4.3 MVC");
-		return "welcome";
-	}
-	
 	@RequestMapping(value = "/thymeleaf", method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("recipient", "World");
