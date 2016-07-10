ee218ba27ee56c362c6ab0632dfc8edf5aa1d498
Karthik <krishnakarthik.adk@gmail.com>
2016-07-10 23:47:31 +0530

    Form Submission in Spring MVC and Thymeleaf

--- track-my-expense/src/main/java/com/dsk/trackmyexpense/controller/HelloWorldController.java
+++ track-my-expense/src/main/java/com/dsk/trackmyexpense/controller/HelloWorldController.java
@@ -2,8 +2,11 @@ package com.dsk.trackmyexpense.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dsk.trackmyexpense.model.TrackMyExpense;
 
 /**
  * 
@@ -16,7 +19,14 @@ public class HelloWorldController {
 
 	@RequestMapping(value = "/thymeleaf", method = RequestMethod.GET)
     public String index(Model model) {
-        model.addAttribute("recipient", "World");
+        model.addAttribute("trackmyexpense", new TrackMyExpense());
         return "index";
     }
+	
+	@RequestMapping(value="/thymeleaf", method=RequestMethod.POST)
+	public String submitExpense(@ModelAttribute TrackMyExpense trackMyExpense, Model model)
+	{
+		model.addAttribute("trackmyexpense", trackMyExpense);
+		return "result";
+	}
 }
