a7a275b5f37380e8d5fa5e9fee5d9f219c408179
Karthik <krishnakarthik.adk@gmail.com>
2016-08-08 23:42:06 +0530

    Application Constants

---
+++ track-my-expense/src/main/java/com/dsk/trackmyexpense/constants/TrackMyExpenseConstants.java
@@ -0,0 +1,12 @@
+package com.dsk.trackmyexpense.constants;
+/**
+ * 
+ * @author Krishna
+ *
+ */
+public class TrackMyExpenseConstants {
+	public static final String DATABASE_DRIVER = "database.driver";
+	public static final String DATABASE_URL = "database.url";
+	public static final String DATABASE_USERNAME = "database.username";
+	public static final String DATABASE_PASSWORD = "database.password";
+}
