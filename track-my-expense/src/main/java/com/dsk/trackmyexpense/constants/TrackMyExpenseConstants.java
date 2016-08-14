73e19b5e3215bf51382358ba5d55de6b6cc2be36
Karthik <krishnakarthik.adk@gmail.com>
2016-08-15 02:32:52 +0530

    Added few more application constants

--- track-my-expense/src/main/java/com/dsk/trackmyexpense/constants/TrackMyExpenseConstants.java
+++ track-my-expense/src/main/java/com/dsk/trackmyexpense/constants/TrackMyExpenseConstants.java
@@ -5,8 +5,31 @@ package com.dsk.trackmyexpense.constants;
  *
  */
 public class TrackMyExpenseConstants {
+	// Database Property CONSTANTS
 	public static final String DATABASE_DRIVER = "database.driver";
 	public static final String DATABASE_URL = "database.url";
 	public static final String DATABASE_USERNAME = "database.username";
 	public static final String DATABASE_PASSWORD = "database.password";
+	
+	// Drop Down menu CONSTANTS for index.html
+	public static final String GROCERIES = "Groceries";
+	public static final String FUEL = "Fuel";
+	public static final String MAINTENANCE = "Maintenance";
+	public static final String ENTERTAINMENT = "Entertainment";
+	public static final String UTILITY_BILLS = "UtilityBills";
+	public static final String VEGETABLES = "Vegetables";
+	public static final String MOBILE_RECHARGE = "MobileRecharge";
+	public static final String PERSONAL_EXPENSE = "PersonalExpense";
+	public static final String SAVINGS = "Savings";
+	public static final String TOUR_VACATION = "Tour/Vacation";
+	public static final String SHOPPING = "Shopping";
+	public static final String VEHICLE_EXPENSE = "VehicleExpense";
+	public static final String TRANSPORT = "Transport";
+	public static final String MISCELLANEOUS = "Miscellaneous";
+	
+	public static final String CASH = "Cash";
+	public static final String ONLINE_TRANSFER = "OnlineTransfer";
+	public static final String NET_BANKING = "NetBanking";
+	public static final String CREDIT_CARD = "CreditCard";
+	
 }
