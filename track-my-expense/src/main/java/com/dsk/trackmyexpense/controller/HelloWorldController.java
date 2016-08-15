ae5c8173107a7a151307ba0e39f898bd52255f9f
Karthik <krishnakarthik.adk@gmail.com>
2016-08-15 23:58:55 +0530

    Introduced session attributes for drop down menu in index.html

--- track-my-expense/src/main/java/com/dsk/trackmyexpense/controller/HelloWorldController.java
+++ track-my-expense/src/main/java/com/dsk/trackmyexpense/controller/HelloWorldController.java
@@ -1,11 +1,14 @@
 package com.dsk.trackmyexpense.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.dsk.trackmyexpense.model.TrackMyExpense;
 import com.dsk.trackmyexpense.service.ITrackMyExpenseService;
@@ -16,17 +19,30 @@ import com.dsk.trackmyexpense.service.ITrackMyExpenseService;
  *
  */
 @Controller
+@SessionAttributes({"itemCategories", "paymentOptions"})
 @RequestMapping("/")
 public class HelloWorldController {
 
 	@Autowired(required=true)
 	private ITrackMyExpenseService expenseService;
 	
+	@ModelAttribute("itemCategories")
+	public List<String> populateItemCategories()
+	{
+		System.out.println("In populateItemCategories()");
+		return new TrackMyExpense().getListOfCategories();
+	}
+	
+	@ModelAttribute("paymentOptions")
+	public List<String> populatePaymentOptions()
+	{
+		System.out.println("In populatePaymentOptions()");
+		return new TrackMyExpense().getPaymentOptions();
+	}
+	
 	@RequestMapping(value = "/thymeleaf", method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("trackmyexpense", new TrackMyExpense());
-        model.addAttribute("allCategories", new TrackMyExpense().getListOfCategories());
-        model.addAttribute("paymentOptions", new TrackMyExpense().getPaymentOptions());
         
         return "index";
     }
