cdba5597c5f360043e732b7d14a48bfa78b43823
Karthik <krishnakarthik.adk@gmail.com>
2016-11-12 23:38:24 +0530

    Introduced Request Logging Filter

---
+++ track-my-expense/src/main/java/com/dsk/trackmyexpense/filters/RequestLoggingFilter.java
@@ -0,0 +1,53 @@
+package com.dsk.trackmyexpense.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.core.annotation.Order;
+
+/*@WebFilter(
+			filterName="RequestLogginFilter", 
+			urlPatterns="/thymeleaf", 
+			description="This filter logs remote address from where the requests originate"
+		  )*/
+
+public class RequestLoggingFilter implements Filter{
+
+	private static final Logger LOGGER = LogManager.getLogger(RequestLoggingFilter.class);
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest requestReceived = (HttpServletRequest) request;
+		
+		// Log the request URI received
+		LOGGER.warn("Request URI Received ----> " + requestReceived.getRequestURI());
+		
+		// Log the IP address from where the request originated
+		LOGGER.warn("Request originated from: IP Address ----> " + request.getRemoteAddr());
+		filterChain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		LOGGER.info("Calling request logging filter to service.");
+	}
+
+}
